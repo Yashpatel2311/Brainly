8c3c424e29bc6a57272b21696738daaf6ee70104
Yash Patel <theyashpatel2311@gmail.com>
2025-02-20 01:10:50 +0530

    8 commits

--- Brainly/src/index.ts
+++ Brainly/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/v1/signup", async (req, res) => {
+app.post("/", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
